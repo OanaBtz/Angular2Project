0f158f3b0ba9a359c29116b630892a59ffed15a3
dandocan <danx4526@stud.kea.dk>
2017-05-20 14:52:02 +0200

    coaie, nu-i ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Game } from './game';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,9 +10,24 @@ export class AppComponent {
   game: Game = {
     id: 1,
     name: "Catan"
+  };
+  games=GAMES;
+  selectedGame: Game;
+  onSelect(game: Game): void{
+    this.selectedGame = game;
   }
 }
-export class Game {
-  id: number;
-  name: string;
-}
+
+const GAMES: Game[] = [
+  { id: 1, name: "Catan" },
+  { id: 2, name: "Risk" },
+  { id: 3, name: "Game of Thrones" },
+  { id: 4, name: "Axis and Allies" },
+  { id: 5, name: "Ticket to ride" },
+  { id: 6, name: "Love Letter" },
+  { id: 7, name: "King of Tokyo" },
+  { id: 8, name: "Flux" },
+  { id: 9, name: "Monopoly" },
+];
+
+
