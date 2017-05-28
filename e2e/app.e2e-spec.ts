036b019a7c783bad5fdd92a6eb673b9ef0ec2d3a
DESKTOP-84KO0O3\botez <oana.btzt@gmail.com>
2017-05-28 09:19:55 +0200

    testing components

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,77 @@
 import { Angular2ExamProjectPage } from './app.po';
+import { browser, element, by} from 'protractor';  
 
-describe('angular2-exam-project App', () => {
+describe('angular2-exam-project app', () => {
   let page: Angular2ExamProjectPage;
 
   beforeEach(() => {
     page = new Angular2ExamProjectPage();
   });
 
-  it('should display message saying app works', () => {
+  it('the title of the page should be Nerd', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Nerd');
   });
+
+
+////////////////////all tests related to all-games page
+
+
+  it("the name of the game in all-games page should be defined", () => {
+    browser.get('/all-games');
+    let name = element(by.css('div.name'));
+    expect(name).toBeDefined();
+
+  });
+
+  it("the description of the game in all-games page should be defined", () => {
+    browser.get('/all-games');
+    let description = element(by.css('div.description'));
+    expect(description).toBeDefined();
+  });
+
+
+  // it("the game should be able to be deleted", () => {
+  //   // let firstGame = element.all(by.css(".games div.game")).get(4);
+  //   let deleteButton = element.all('.games .game .delete').first();
+
+  //   deleteButton.click();
+
+  //   // let numberOfGamesAfterDelete = element.all(by.css(".games")).count();
+  //   expect(deleteButton).toBeDefined;
+
+  // })
+
+/////////////////all tests related to add-game page
+  
+  it("in the add-game page the name of the game should be empty", () => {
+    browser.get('/add-game');
+    let gameName = element(by.css('div.name'));
+    let text = gameName.getText();
+    expect(gameName.getText()).toEqual('');
+  });
+
+
+  it("should be able to add a new game", ()=> {
+    browser.get('/add-game');
+
+    let newGameName = element(by.css('input.inputName'));
+    newGameName.sendKeys('Catan-expansion');
+
+    let newGameDescription = element(by.css('input.inputDescription'));
+    newGameDescription.sendKeys("A great multiplayer game :D ");
+
+    let newGameUrl = element(by.css('input.inputUrl'));
+    newGameUrl.sendKeys('http://hobbyshop.lt/23627-thickbox_default/katan-papildymas-miestai-ir-riteriai-cities-knights.jpg')
+    
+    
+    let saveButton = element(by.css('button.button'));
+    saveButton.click();
+    
+  });
+
+  
+
+
+// browser.pause();
 });
